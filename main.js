af39e93b5528d28a5c80b1f7c286402aa31ccd67
Alberto Sánchez P <alsape1a@gmail.com>
2022-10-12 22:14:24 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+/* Desafío: javaScript
+
+Nombre de Desafío: introduccion_js
+
+Instrucciones
+
+Crea una lista con los siguientes elementos:*/
+
+/*- //Tu nombre (string)
+- //Tu apellido (string)
+- //Tu edad (number)
+- //¿Eres desarrollador de aplicaciones web? (boolean)
+- //Tu fecha de nacimiento (Date)
+- //Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+- //Tu pasatiempos favoritos (Array)
+- //Declara una funcion miActividadFavorita la cual retorne un string con la accion que mas te guste realizar 
+*/
+
+var Alberto191181 = [
+    nombre= Alberto,
+    apellido= Sánchez,
+    edad= 40,
+    Eres_desarrollador_de_aplicaciones_web= true,
+    veryday= 19-11-1981]
+    console.log=(nombre)
+var 
+libro_favorito=["la_culpa es de la vaca",
+autor= "Camilo Cruz",
+url= "https://www.academia.edu/10651735/LIBRO_LA_CULPA_ES_DE_LA_VACA",
+
+] 
+var pasatiempos="leer libros de superacion personal"
+var miActividadFavorita="hacer deporte"
